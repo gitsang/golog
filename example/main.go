9224545047a25f53437cb9ecf5c5cf182558f355
陈司行 <chensx@yealink.com>
2020-11-13 17:59:26 +0800

    example: add example

---
+++ example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	log "github.com/gitsang/golog"
+	"go.uber.org/zap"
+)
+
+func logWithoutInit() {
+	log.Info("test without init")
+}
+
+func logWithInit() {
+	log.InitLogger(zap.InfoLevel, "log")
+	log.Info("test with init")
+}
+
+func main() {
+	logWithoutInit()
+	logWithInit()
+}
