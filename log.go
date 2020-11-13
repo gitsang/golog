b47ba20d82a6ba29d9b42792452abeb10e003135
陈司行 <chensx@yealink.com>
2020-11-13 18:00:23 +0800

    update: add function key in order to show caller function

--- log.go
+++ log.go
@@ -39,6 +39,7 @@ func getEncoder() zapcore.Encoder {
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderConfig.FunctionKey = "func"
 	return zapcore.NewConsoleEncoder(encoderConfig)
 }
 
